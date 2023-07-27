46832b61585d24f1767c8fbd9622e09c9f548bea
ksh03003 <74225015+kwakchaewon@users.noreply.github.com>
2023-07-27 15:25:49 +0900

    feat: blog admin 페이지

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import *
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email']
+    list_display_links= ['username', 'email']
+
+
+@admin.register(BoardCategories)
+class BoardCategoriesAdmin(admin.ModelAdmin):
+    list_display = ['category_type', 'category_code', 'category_name']
+    list_display_links= ['category_type', 'category_code', 'category_name']
+
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ['title', 'conntents', 'user', 'created_date']
+    list_display_links= ['title', 'conntents']
+
+
+@admin.register(BoardReplies)
+class BoardRepliesAdmin(admin.ModelAdmin):
+    list_display = ['contents', 'user', 'created_date']
+    list_display_links= ['contents']
+
+
+@admin.register(BoardLikes)
+class BoardLikesAdmin(admin.ModelAdmin):
+    list_display = ['board', 'user', 'created_date']
+    list_display_links= ['board', 'user', 'created_date']
